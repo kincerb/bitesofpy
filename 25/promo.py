a52a179321b9fa8dea009df528716a696255f574
kincerb <dev.bkincer@gmail.com>
2020-02-08 00:26:00 -0500

    Bite 25 complete

---
+++ 25/promo.py
@@ -0,0 +1,47 @@
+import random
+
+BITES = {6: 'PyBites Die Hard',
+         7: 'Parsing dates from logs',
+         9: 'Palindromes',
+         10: 'Practice exceptions',
+         11: 'Enrich a class with dunder methods',
+         12: 'Write a user validation function',
+         13: 'Convert dict in namedtuple/json',
+         14: 'Generate a table of n sequences',
+         15: 'Enumerate 2 sequences',
+         16: 'Special PyBites date generator',
+         17: 'Form teams from a group of friends',
+         18: 'Find the most common word',
+         19: 'Write a simple property',
+         20: 'Write a context manager',
+         21: 'Query a nested data structure'}
+bites_done = {6, 10, 16, 18, 21}
+
+
+class NoBitesAvailable(Exception):
+    pass
+
+
+class Promo:
+
+    def __init__(self, bites_done=bites_done):
+        self.bites_done = bites_done
+
+    def _bites_available(self):
+        return [avail for avail in list(BITES.keys()) if avail not in self.bites_done]
+
+    def _pick_random_bite(self):
+        try:
+            return random.choice(self._bites_available())
+        except IndexError:
+            raise NoBitesAvailable
+
+    def new_bite(self):
+        try:
+            bite = self._pick_random_bite()
+        except NoBitesAvailable:
+            pass
+        else:
+            self.bites_done.add(bite)
+            return bite
+
