5cfd21f9edeec47774cb2991bc2968dea25c5a12
kincerb <dev.bkincer@gmail.com>
2020-02-09 22:48:41 -0500

    Bite 27 complete

--- 27/omdb.py
+++ 27/omdb.py
@@ -13,17 +13,19 @@ def get_single_comedy(movies: list) -> str:
     """return the movie with Comedy in Genres"""
     for movie in movies:
         if 'Comedy' in movie.get('Genre'):
-            return movie
+            return movie.get('Title')
 
 
 def get_movie_most_nominations(movies: list) -> str:
     """Return the movie that had the most nominations"""
-    votes = 0
-    most_votes = None
+    max_votes = 0
+    most_votes_movie = None
     for movie in movies:
-        if int(movie.get('imdbVotes')) > votes:
-            most_votes = movie
-    return most_votes
+        movie_votes = int(movie.get('imdbVotes').replace(',', ''))
+        if movie_votes > max_votes:
+            most_votes_movie = movie
+            max_votes = movie_votes
+    return most_votes_movie.get('Title')
 
 
 def get_movie_longest_runtime(movies: list) -> str:
@@ -31,6 +33,8 @@ def get_movie_longest_runtime(movies: list) -> str:
     max_runtime = 0
     longest_movie = None
     for movie in movies:
-        if int(movie.get('Runtime').split()[0]) > max_runtime:
+        movie_runtime = int(movie.get('Runtime').split()[0])
+        if movie_runtime > max_runtime:
             longest_movie = movie
-    return longest_movie
+            max_runtime = movie_runtime
+    return longest_movie.get('Title')
